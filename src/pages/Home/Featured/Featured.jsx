32639ba9a7e6bc245af5e57f8b8d66312357bcdf
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-01-02 07:11:38 -0500

    first commit

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css'
+
+const Featured = () => {
+    return (
+       <div className='featured-item bg-fixed text-white pt-8 my-20'>
+        <SectionTitle subHeading={"check it out"} heading={"Featured Item"}/>
+        <div className='md:flex justify-center items-center bg-opacity-60 pb-20 pt-12 px-36'>
+            
+            <div>
+                <img src={featuredImg} alt="" />
+            </div>
+            <div className='md:ml-10'>
+                <p>Aug 20, 2029</p>
+                <p className='uppercase'>Where can i get some?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam pariatur quae aliquid quos illo quas a officiis fuga itaque, adipisci delectus? Voluptate quae, minima, itaque, fugit eum nam odio placeat eaque inventore dolorum quam. Officia sequi in ab voluptas consequuntur et. Magni, tempora commodi ducimus ad nihil eius saepe sed?</p>
+                <button className='btn btn-outline border-0 border-b-4 '>Order Now</button>
+            </div>
+        </div>
+       </div>
+        
+       
+    );
+};
+
+export default Featured;
